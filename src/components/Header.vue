ea162c75caa406cce7f2214def5d59080f3ac723
Francisco Daines <francisco.daines@2brains.cl>
2020-08-20 21:04:36 -0400

    Applies walmart styles to search page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,68 @@
 <template>
-  <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>HEADER</h1>
+<section>
+  <div class="d-flex">
+    <div id="header" class="header-info">
+      <div class="header-wrapper header-info-expanded">
+        <span>
+          Servicio al cliente de Lider.cl: WhatsApp <a href="#">+56957211492</a>
+          | Horario: de lunes a viernes de 8:00 a 22:00 y sábado, domingo y festivos de 9:00 a 22:00
+        </span>
+      </div>
+    </div>
+    <div class="site-header compact" style="background-color: rgb(0, 113, 206);">
+      <div class="header-wrapper">
+        <div class="logo">
+          <a href="#">
+            <img alt="Lider Logo" src="https://www.lider.cl/catalogo/images/lider-logo.svg" style="width: 152px; height: 35px; margin-top: 12px; margin-bottom: 6px;">
+          </a>
+        </div>
+        <div class="menu-icon-container">
+          <div style="cursor: pointer;">
+            <i class="zmdi zmdi-menu menu-icon"></i>
+            <div class="show-category">Categorías</div>
+          </div>
+        </div>
+        <div style="width: 100%; margin: 0px 3%;">
+          <div class="search-width">
+            <div class="search-box-container-header" style="padding-top: 10px;">
+              <section>
+                <div class="ais-InstantSearch__root">
+                  <div class="ais-InstantSearch__root">
+                    <div class="ais-SearchBox">
+                      <form class="ais-SearchBox-form" role="search">
+                        <input type="search" placeholder="¿Qué estás buscando?" maxlength="512" class="ais-SearchBox-input" value="">
+                        <button type="submit" title="Submit your search query." class="ais-SearchBox-submit search-input-icons">
+                          <svg class="ais-SearchBox-submitIcon" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 40 40">
+                            <path d="M26.804 29.01c-2.832 2.34-6.465 3.746-10.426 3.746C7.333 32.756 0 25.424 0 16.378 0 7.333 7.333 0 16.378 0c9.046 0 16.378 7.333 16.378 16.378 0 3.96-1.406 7.594-3.746 10.426l10.534 10.534c.607.607.61 1.59-.004 2.202-.61.61-1.597.61-2.202.004L26.804 29.01zm-10.426.627c7.323 0 13.26-5.936 13.26-13.26 0-7.32-5.937-13.257-13.26-13.257C9.056 3.12 3.12 9.056 3.12 16.378c0 7.323 5.936 13.26 13.258 13.26z"></path>
+                          </svg>
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+
+        <div class="cart-supermarket-width">
+          <div class="cart-container" style="background-color: rgb(4, 30, 66);">
+            <li class="list-inline-item cart-dropdown dropdown nav-item">
+              <a aria-haspopup="true" href="#" class="p-0 nav-link" aria-expanded="false">
+                <div id="shoppingCart" class="d-flex justify-content-between shopping-cart-images">
+                  <img alt="cart" src="https://www.lider.cl/catalogo/images/lider-cart.svg" width="29" height="25">
+                  <div class="cart-quantity">
+                    <div id="quantityNumber" class="quantity-number">0</div>
+                  </div>
+                </div>
+              </a>
+            </li>
+          </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
 </template>
 
 <script>
--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,7 +1,121 @@
 <template>
-  <section class="content">
-  Desafío Walmart
-  </section>
+  <div class="app-container">
+    <div class="rct-app-content">
+      <div class="rct-page">
+        <div class="content-wrapper">
+          <div class="shop-wrapper">
+            <div class="rct-page">
+              <div class="rct-page-content">
+                <div class="shop-wrapper">
+                  <div class="shop-wrapper" style="margin-right: 5px;">
+                    <div class="d-flex" style="margin-top: -24px;">
+                      <div class="col-lg-5 col-md-4 d-none d-md-block pt-5 col-padding-12">
+                        <p class="mb-10 pt-20" style="font-size: 18px;">
+                          <b class="text-capitalize"> Resultados para búsqueda</b>: SEARCH_PATTERN
+                        </p>
+                      </div>
+                      <div class="col-lg-7 col-md-8 col-sm-12">
+                        <div>
+                          <div class="dropdown">
+                            <div class="dropdown">
+                              <div data-toggle="dropdown" aria-expanded="false" aria-haspopup="true" class="category-order-dropdown">
+                                Ordenar por: precio
+                                <span class="pull-right">
+                                  <img alt="open-arrow" width="5" src="https://www.lider.cl/catalogo/images/arrow_mobile.svg">
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="d-flex mb-60">
+                      <div class="col-lg-12 col-md-12 col-sm-12 col-padding-12">
+                        <div class="shop-content">
+                          <div style="" class="ais-Hits">
+                            <ul class="ais-Hits-list">
+                              <li class="ais-Hits-item">
+                                <div class="">
+                                  <div class="rct-block  product-block flex-column justify-content-between overflow-hidden bg-white">
+                                    <a href="/catalogo/product/sku/982651" style="text-decoration: none; height: 100%; display: inline; background-color: white;">
+                                      <div id="982651" class="overlay-wrap overflow-hidden bg-white" style="display: flex; align-items: center; justify-content: center; max-height: 220px; width: 100%;">
+                                        <div class="text-center">
+                                          <div class="product-image-shadow"></div>
+                                          <div class="d-flex labels-container"></div>
+                                          <div class="d-flex cyber-lider-cards-container"></div>
+                                          <div id="image" class="bg-white">
+                                            <div>
+                                              <img id="lazy-img" class="img-hover-zoom img-fluid m-auto " alt="Mademsa - Refrigerador Frío Directo 212 Litros /&nbsp;Nordik 2200" src="https://images.lider.cl/wmtcl?source=url[file:/productos/982651a.jpg]&amp;scale=size[300x300]&amp;&amp;sink" style="filter: unset; min-width: 50px; max-height: 220px;">
+                                            </div>
+                                          </div>
+                                          <div></div>
+                                        </div>
+                                      </div>
+                                      <div class=" product-info">
+                                        <div class="d-flex text-left product-description  ">
+                                          <div>
+                                            <span class="" style="font-weight: bold; color: rgb(0, 0, 0);">PRODUCT_BRAND</span>
+                                            <span> PRODUCT DESCRIPTION CAN BE VERY VERY VERY LARGE AND CONTINUES WRITING</span>
+                                          </div>
+                                        </div>
+                                        <div class="mb-12">
+                                          <div class="walmart-sales-price d-flex " style="display: flex; align-items: center;">
+                                            $PRODUCT_PRICE
+                                            <div class="walmart-discount-percentage-card" style="margin-top: 1px; margin-left: 6px;">DISCOUNT%</div>
+                                          </div>
+                                          <div class="d-flex">
+                                            <span class="walmart-reference-price">$ORIGINAL_PRICE</span>
+                                          </div>
+                                        </div>
+                                        <div>
+                                          <div class="walmart-add-cart-button-container" style="justify-content: center; width: 95%;">
+                                            <span id="addProductToCart" class="ProductCardButton__AddToCartButton-sc-1r8ezq4-0 iRLHuO walmart-add-cart-button pt-5 text-center cp-no-select">Agregar</span>
+                                          </div>
+                                          <div class="walmart-add-cart-button-container-alert"></div>
+                                        </div>
+                                      </div>
+                                    </a>
+                                  </div>
+                                </div>
+                              </li>
+                            </ul>
+                          </div>
+                          <div class="pagination-container">
+                            <div class="pagination-wmt">
+                              <ul class="ais-Pagination-list">
+                                <li class="ais-Pagination-item ais-Pagination-item--previousPage pagination-disabled-arrows">
+                                  <a tabindex="0" role="button" aria-disabled="true">&lt;</a>
+                                </li>
+                                <li class="ais-Pagination-item ais-Pagination-item--page ais-Pagination-item--selected">
+                                  <a role="button" class="ais-Pagination-link" tabindex="0" aria-label="Page 1 is your current page" aria-current="page">1</a>
+                                </li>
+                                <li class="ais-Pagination-item ais-Pagination-item--page">
+                                  <a role="button" class="ais-Pagination-link" tabindex="0" aria-label="Page 2">2</a>
+                                </li>
+                                <li class="ais-Pagination-item ais-Pagination-item--page">
+                                  <a role="button" class="ais-Pagination-link" tabindex="0" aria-label="Page 3">3</a>
+                                </li>
+                                <li class="ais-Pagination-item ais-Pagination-item--page">
+                                  <a role="button" class="ais-Pagination-link" tabindex="0" aria-label="Page 4">4</a>
+                                </li>
+                                <li class="ais-Pagination-item ais-Pagination-item--nextPage">
+                                  <a tabindex="0" role="button" aria-disabled="false">&gt;</a>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
