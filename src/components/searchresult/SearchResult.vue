c9d7e9fe25ab53a4711b62ef372f72343feaca82
Francisco Daines <francisco.daines@2brains.cl>
2020-08-21 15:45:58 -0400

    Extracts search result section to an isolated component

---
+++ src/components/searchresult/SearchResult.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="shop-wrapper" style="margin-right: 5px;">
+        <div class="d-flex" style="margin-top: -24px;">
+            <div class="col-lg-5 col-md-4 d-none d-md-block pt-5 col-padding-12">
+            <p class="mb-10 pt-20" style="font-size: 18px;">
+                <b class="text-capitalize"> Resultados para búsqueda</b>: {{ searchPattern }}
+            </p>
+            </div>
+            <div class="col-lg-7 col-md-8 col-sm-12">
+            <div>
+                <div class="dropdown">
+                <div class="dropdown">
+                    <div data-toggle="dropdown" aria-expanded="false" aria-haspopup="true" class="category-order-dropdown">
+                    Ordenar por: precio
+                    <span class="pull-right">
+                        <img alt="open-arrow" width="5" src="https://www.lider.cl/catalogo/images/arrow_mobile.svg">
+                    </span>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="d-flex mb-60">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-padding-12">
+            <div class="shop-content">
+                <div style="" class="ais-Hits">
+                <ul class="ais-Hits-list">
+                    <li v-for="(product, index) in products" :key="index" class="ais-Hits-item">
+                    <product-card :product="product" />
+                    </li>
+                </ul>
+                </div>
+                <div class="pagination-container">
+                <paginator :totalPages="totalPages" :currentPage="currentPage" />
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import ProductCard from '../product/ProductCard.vue'
+import Paginator from '../paginator/Paginator.vue'
+
+export default {
+    name: 'SearchResult',
+    components: {
+        ProductCard,
+        Paginator
+    },
+    props: [
+        "totalPages",
+        "currentPage",
+        "products",
+        "searchPattern"
+    ]
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -7,45 +7,7 @@
             <div class="rct-page">
               <div class="rct-page-content">
                 <div class="shop-wrapper">
-                  <div class="shop-wrapper" style="margin-right: 5px;">
-                    <div class="d-flex" style="margin-top: -24px;">
-                      <div class="col-lg-5 col-md-4 d-none d-md-block pt-5 col-padding-12">
-                        <p class="mb-10 pt-20" style="font-size: 18px;">
-                          <b class="text-capitalize"> Resultados para búsqueda</b>: SEARCH_PATTERN
-                        </p>
-                      </div>
-                      <div class="col-lg-7 col-md-8 col-sm-12">
-                        <div>
-                          <div class="dropdown">
-                            <div class="dropdown">
-                              <div data-toggle="dropdown" aria-expanded="false" aria-haspopup="true" class="category-order-dropdown">
-                                Ordenar por: precio
-                                <span class="pull-right">
-                                  <img alt="open-arrow" width="5" src="https://www.lider.cl/catalogo/images/arrow_mobile.svg">
-                                </span>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="d-flex mb-60">
-                      <div class="col-lg-12 col-md-12 col-sm-12 col-padding-12">
-                        <div class="shop-content">
-                          <div style="" class="ais-Hits">
-                            <ul class="ais-Hits-list">
-                              <li v-for="(product, index) in products" :key="index" class="ais-Hits-item">
-                                <product-card :product="product" />
-                              </li>
-                            </ul>
-                          </div>
-                          <div class="pagination-container">
-                            <paginator :totalPages="5" :currentPage="2" />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <search-result :products="products" :search-pattern="pattern" :total-pages="totalPages" :current-page="currentPage" />
                 </div>
               </div>
             </div>
@@ -57,17 +19,18 @@
 </template>
 
 <script>
-import ProductCard from '../components/product/ProductCard.vue'
-import Paginator from '../components/paginator/Paginator.vue'
+import SearchResult from '../components/searchresult/SearchResult'
 
 export default {
   name: 'SearchPage',
   components: {
-    ProductCard,
-    Paginator
+    SearchResult
   },
   data() {
     return {
+      pattern: 'HolaMundo',
+      totalPages: 10,
+      currentPage: 2,
       products: [
         {
           brand: 'Marca X',
