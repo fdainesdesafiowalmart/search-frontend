13dfe1930a896f264144069f52120e9e20cde602
Francisco Daines <francisco.daines@2brains.cl>
2020-08-22 16:13:06 -0400

    Adds apikey for backend for frontend

--- src/clients/backend.js
+++ src/clients/backend.js
@@ -1,15 +1,23 @@
 import axios from 'axios';
 
-const baseEndpoint = 'http://localhost:8332';
+const baseEndpoint = 'http://localhost:8090';
 
 const instance = axios.create({
   baseURL: baseEndpoint,
   timeout: 5000
 });
 
+const resolveBffApiKeyHeaders = () => {
+  const headers = {}
+  headers['x-walmart-search-bff-key'] = 'SEARCH_BFF_API_KEY'
+
+  return headers
+}
+
 const genericGetRequest = (endpoint, params) => instance.get(endpoint, {
   timeout: 10000,
-  params: params || {}
+  params: params || {},
+  headers: resolveBffApiKeyHeaders()
 }).then(result => result.data);
 
 const searchProducts = params => genericGetRequest('/search', params);
